d14d317db233556f901e959e6fcc07aac4664f10
Idriss <namidid2.0@gmail.com>
2024-10-28 16:38:13 +0100

    Domino piling

---
+++ Domino Piling/Domino Piling/Domino Piling.cpp
@@ -0,0 +1,16 @@
+// Domino Piling.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+#include <string>
+int main()
+{
+	int n,m;
+	std::cin >> n >> m;
+	std::cout << (n * m) / 2;
+
+
+
+	return 0;
+}
+
